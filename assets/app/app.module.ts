033114d7cac56b6c80a97ea0612396c0390f2314
Chris Cantu <pinion31@gmail.com>
2018-05-28 20:36:49 -0500

    removed extra components

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,20 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { MainNavBarComponent } from './main-nav-bar/main-nav-bar.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    MainNavBarComponent,
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
